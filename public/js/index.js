1bd3f566114c6811474c402898278c96b78ee251
michael emmons <memmons18@gmail.com>
2018-05-02 19:37:08 -0400

    added background images and timeintervals for changing the images

--- public/js/index.js
+++ public/js/index.js
@@ -1,12 +1,35 @@
 let a =0;
 let b =0;
+var imageCount=0;
 
+
+
+var backgroundImages={
+    img:["'../wallpaper/restPic5.jpg'","../wallpaper/restPic2.jpg","../wallpaper/restPic3.jpg","../wallpaper/restPic4.jpg","../wallpaper/restPic6.jpg"],
+}
 
 $(".addRest").on("click",function(){
     populateAddRest()
     
 })
 
+imageCount = Math.ceil(Math.random()*backgroundImages.img.length);
+
+$("body").css("background-image","url("+backgroundImages.img[imageCount]+")")
+console.log(imageCount)
+
+
+setInterval(function(){
+    $("body").css("background-image","url("+backgroundImages.img[imageCount]+")")
+    checkCount()
+},1000 *30)
+
+function checkCount(){
+    imageCount++;
+    if(imageCount == backgroundImages.img.length ){
+        imageCount=0
+    }
+}
 
 
 function toPage(data){
